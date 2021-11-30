5e4f5d71e64b3a78cd877402fc953d46efd5ff79
Tammy Yang <tammy.yang@coinbase.com>
2021-11-30 21:23:27 +0000

    smart contract exercise

---
+++ contracts/DecentralisedWill.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract DecentralisedWill {
+  
+// Get distrubtion list - address vs % mapping
+function getMapping() public {}
+
+// Check transaction history of the owner's accounts
+// If there are any activities in the last 12-month, revert contract
+// else, process with the contract
+function checkActivityHisory() public {}
+
+// Get price oracle for assets in owner's account
+function getPriceOracle() public {}
+
+// Calculate total USD value of assets in Owner's account
+function getValueUSD() public {}
+
+// Distribute asset based on will
+function distributeAsset() public {}
+
+}
+
+
+
+
